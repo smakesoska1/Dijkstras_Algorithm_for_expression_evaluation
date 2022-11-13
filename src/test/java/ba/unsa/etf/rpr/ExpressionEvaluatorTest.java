865dffa68775f6479668c24fb75b0c8564d9ae9b
smakesoska1 <smakesoska1@etf.unsa.ba>
2022-11-13 14:40:26 +0100

    correction of test message

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ExpressionEvaulatorTest {
+public class ExpressionEvaluatorTest {
 
 
         @Test

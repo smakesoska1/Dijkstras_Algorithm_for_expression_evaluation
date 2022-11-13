e5eeeb2dcc59f69a777f8e95657376a98470db6d
smakesoska1 <smakesoska1@etf.unsa.ba>
2022-11-13 12:27:57 +0100

    added main method and 4 unit tests

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -8,6 +8,46 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        //String proba = "( 1 + ( 5 * 2 ) )";
+
+        String newString = "";
+        int size = args.length;
+        boolean korektno = true;
+
+        for (int i = 0; i < args.length; i++) {
+            if (i == size - 1) {
+                newString = newString + args[i];
+            } else {
+                newString = newString + args[i] + " ";
+            }
+        }
+
+
+        //System.out.printf("Ispisano je:" + novi);
+        //System.out.println(novi.length());
+        //System.out.println(proba.length());
+
+        /*for (int i = 0; i < velicina; i++) {
+            if (i % 2 == 1 && proba.charAt(i) != ' ') {
+                korektno = false;
+                break;
+            }
+            if (i == 0 && proba.charAt(i) != '(') {
+                korektno = false;
+                break;
+            }
+        }
+
+        if (korektno) {
+            System.out.println("Sve ok");
+        } else {
+            throw new RuntimeException("Nije sve ok");
+        }*/
+
+        Double result=ExpressionEvaluator.evaluate(newString);
+
+        System.out.println(result);
+
     }
 }
--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaulatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaulatorTest.java
@@ -1,4 +1,37 @@
 package ba.unsa.etf.rpr;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ExpressionEvaulatorTest {
-}
+
+
+        @Test
+        public void test1(){
+            String string=new String("( ( 1 + 2 ) * 3 )");
+            Assertions.assertEquals(9.0,ExpressionEvaluator.evaluate(string));
+        }
+
+
+        @Test
+        public void test2(){
+            String string=new String("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )");
+            Assertions.assertEquals(101.0,ExpressionEvaluator.evaluate(string));
+        }
+
+        @Test
+        public void test3(){
+            String string=new String("( ( 0 + 0 ) * 3 )");
+            Assertions.assertEquals(0.0,ExpressionEvaluator.evaluate(string));
+        }
+
+
+
+        @Test
+        public void test4(){
+            String string=new String("( 1 + ( 5 * 20 ) )");
+            Assertions.assertEquals(101.0,ExpressionEvaluator.evaluate(string));
+        }
+
+    }
+

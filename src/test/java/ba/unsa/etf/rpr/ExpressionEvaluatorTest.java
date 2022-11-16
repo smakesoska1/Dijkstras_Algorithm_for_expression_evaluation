c50017ed1f71af56e269b9f77a051bc5e4673ed4
smakesoska1 <smakesoska1@etf.unsa.ba>
2022-11-16 15:50:22 +0100

    added comments for javadoc

--- src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
+++ src/test/java/ba/unsa/etf/rpr/ExpressionEvaluatorTest.java
@@ -14,8 +14,9 @@ public class ExpressionEvaluatorTest {
      * */
         @Test
         public void test1(){
+            ExpressionEvaluator e=new ExpressionEvaluator();
             String string=new String("( ( 1 + 2 ) * 3 )");
-            Assertions.assertEquals(9.0,ExpressionEvaluator.evaluate(string));
+            Assertions.assertEquals(9.0,e.evaluate(string));
         }
 
     /**
@@ -23,8 +24,9 @@ public class ExpressionEvaluatorTest {
      * */
         @Test
         public void test2(){
+            ExpressionEvaluator e=new ExpressionEvaluator();
             String string=new String("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )");
-            Assertions.assertEquals(101.0,ExpressionEvaluator.evaluate(string));
+            Assertions.assertEquals(101.0,e.evaluate(string));
         }
 
     /**
@@ -32,8 +34,9 @@ public class ExpressionEvaluatorTest {
      * */
         @Test
         public void test3(){
+            ExpressionEvaluator e=new ExpressionEvaluator();
             String string=new String("( ( 0 + 0 ) * 3 )");
-            Assertions.assertEquals(0.0,ExpressionEvaluator.evaluate(string));
+            Assertions.assertEquals(0.0,e.evaluate(string));
         }
 
 
@@ -42,8 +45,9 @@ public class ExpressionEvaluatorTest {
      * */
         @Test
         public void test4(){
+            ExpressionEvaluator e=new ExpressionEvaluator();
             String string=new String("( 1 + ( 5 * 20 ) )");
-            Assertions.assertEquals(101.0,ExpressionEvaluator.evaluate(string));
+            Assertions.assertEquals(101.0,e.evaluate(string));
         }
 
     /**
@@ -51,16 +55,18 @@ public class ExpressionEvaluatorTest {
      * */
     @Test
     public void test5(){
+        App a=new App();
         String string=new String("1 + ( 5 * 20 ) )");
-        Assertions.assertThrows(RuntimeException.class,()->App.validate(string),"There is no first (main) bracket");
+        Assertions.assertThrows(RuntimeException.class,()->a.validate(string),"There is no first (main) bracket");
     }
     /**
      * testing if entered String has no last bracket
      * */
     @Test
     public void test6(){
+        App a=new App();
         String string=new String("( 1 + ( 5 * 20 ) ");
-        Assertions.assertThrows(RuntimeException.class,()->App.validate(string),"There is no last (main) bracket");
+        Assertions.assertThrows(RuntimeException.class,()->a.validate(string),"There is no last (main) bracket");
     }
 
     /**
@@ -68,8 +74,9 @@ public class ExpressionEvaluatorTest {
      * */
     @Test
     public void test7(){
+        App a=new App();
         String string=new String("( 5 + ( 5 +100 ) )");
-        Assertions.assertThrows(RuntimeException.class,()->App.validate(string),"There is no space between operands");
+        Assertions.assertThrows(RuntimeException.class,()->a.validate(string),"There is no space between operands");
     }
 
     /**
@@ -77,8 +84,9 @@ public class ExpressionEvaluatorTest {
      * */
     @Test
     public void test8(){
+        App a=new App();
         String string=new String("( 1 + (( 2 + 3 ) * ( 4 * 5 ) ) )");
-        Assertions.assertThrows(RuntimeException.class,()->App.validate(string),"There is no space behind ( bracket");
+        Assertions.assertThrows(RuntimeException.class,()->a.validate(string),"There is no space behind ( bracket");
     }
 
     /**
@@ -86,8 +94,9 @@ public class ExpressionEvaluatorTest {
      * */
     @Test
     public void test9(){
+        App a=new App();
         String string=new String("( 1 + ( ( 2 + 3 ) * ( 4 * 5 )))");
-        Assertions.assertThrows(RuntimeException.class,()->App.validate(string),"There is no space in front of ) bracket");
+        Assertions.assertThrows(RuntimeException.class,()->a.validate(string),"There is no space in front of ) bracket");
     }
 
 
